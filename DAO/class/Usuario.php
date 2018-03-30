fffd016079912c971ab7c11c4c6be454b18f83f9
Lucas Bastos <lucsbasto@gmail.com>
2018-03-30 19:11:25 -0300

    add getters and setters

--- DAO/class/Usuario.php
+++ DAO/class/Usuario.php
@@ -7,10 +7,50 @@ class Usuario
     private $senha;
     private $dtcadastro;
 
-    public function __construct($login = "", $pass = "")
+    public function __construct($login = "", $pass = "") // recebe login e senha como usuario e seta como "" se não for passado
     {
         $this->setLogin($login);
         $this->setSenha($pass);
+    }
+
+    public function getid()
+    {
+        return $this->id;
+    }
+
+    public function setid($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    public function setLogin($login)
+    {
+        $this->login = $login;
+    }
+
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    public function setSenha($senha)
+    {
+        $this->senha = $senha;
+    }
+
+    public function getDtcadastro()
+    {
+        return $this->dtcadastro;
+    }
+
+    public function setDtcadastro($dtcadastro)
+    {
+        $this->dtcadastro = $dtcadastro;
     }
 
 }
