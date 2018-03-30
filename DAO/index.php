119977d42dfbc0aa3f33aa6aed9bbd8ea97ba56f
Lucas Bastos <lucsbasto@gmail.com>
2018-03-30 19:25:47 -0300

    add arquivo usando todas as funções de Usuario index.php

---
+++ DAO/index.php
@@ -0,0 +1,43 @@
+<?php
+require_once "config.php";
+
+//SELECT POR ID
+//$usuario = new Usuario();
+//$usuario->loadById(22);
+//echo $usuario;
+
+//LISTA COM TODOS
+//$list = Usuario::getList();
+//echo json_encode($list);
+
+//SELECT POR LIKE
+//$search = Usuario::search("o");
+//echo json_encode($search);
+
+// SELECT POR LOGIN E SENHA
+//$login = new Usuario();
+//$login -> login("root", "toor");
+//echo $login;
+
+//INSERT
+//$insert = new Usuario();
+//$insert-> insert("tb_usuarios","test","test");
+
+//UPDATE
+//$update = new Usuario();
+//$update -> update("test", "test", "teste1");
+//$update->loadByLogin("test");
+//echo $update;
+
+//SELECT POR LOGIN
+//$usuario = new Usuario();
+//$usuario->loadByLogin("lucsbasto");
+//echo $usuario;
+
+//DELETE POR LOGIN
+//$delete = new Usuario();
+//echo json_encode(Usuario::getList());
+//$delete -> delete("test");
+//echo json_encode(Usuario::getList());
+
+?>
