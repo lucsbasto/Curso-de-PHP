9558929c6c57cc3daf7268bd8156b817c03d4da9
Lucas Bastos <lucsbasto@gmail.com>
2018-03-30 14:33:15 -0300

    escopo de variaveis no php

---
+++ Variaveis/escopo.php
@@ -0,0 +1,17 @@
+<?php
+
+$nome = "Lucas";
+
+function teste(){
+    global $nome;
+    echo $nome;
+}
+
+function teste2(){
+    $nome = "João";
+    echo $nome." Agora no teste2";
+}
+
+teste();
+teste2();
+?>
