74bceafd6e966833abab7554111854b82825d363
Lucas Bastos <lucsbasto@gmail.com>
2018-03-30 18:18:04 -0300

    usando mkdir pra criar djretk[[

---
+++ diretorios_php/criando_diretorio.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "images";
+
+if(!is_dir($name)){
+    mkdir($name);//cria diretorio
+    echo "Diretorio $name criado com sucesso !";
+}
+else{
+    //rmdir($name); // apaga diretorio
+    echo "Diretorio $name já existe !";
+}
+
+?>
