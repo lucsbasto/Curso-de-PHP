51fd02719b6f87db2b0677933e08fcc14847aceb
Lucas Bastos <lucsbasto@gmail.com>
2018-04-03 17:36:20 -0300

    criando um cerficidado em jpg

---
+++ GD/criando imagens a partir de outra.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'config.php';
+$img = imagecreatefromjpeg('certificado.jpg');
+$black = imagecolorallocate($img, 0,0,0);
+$gray = imagecolorallocate($img, 100,100,100);
+$date = date('d-m-Y');
+imagestring($img,5, 450, 150, 'Certificado', $black);
+imagestring($img,5, 440, 350, 'Lucas Alves', $black);
+imagestring($img,3, 440, 370, "Concluido em ".date('d/m/Y'), $gray);
+header("Content-Type: image/jpeg");
+imagejpeg($img, "certificado-$date.jpg", 100);
+imagedestroy($img);
+
+?>
