d15027b1a29c9e110641929ea51d4dcf61b2dfae
Lucas Bastos <lucsbasto@gmail.com>
2018-04-04 17:03:37 -0300

    arquivos necessários

---
+++ GD/config.php
@@ -0,0 +1,12 @@
+<?php
+
+spl_autoload_register(function ($class_name){
+    $file = "class".DIRECTORY_SEPARATOR."$class_name.php";
+    if(file_exists($file)){
+        require_once "$file";
+    }
+});
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+?>
