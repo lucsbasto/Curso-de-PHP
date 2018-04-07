69ca4137b9c6c912c16202cad2265761f185013f
Lucas Bastos <lucsbasto@gmail.com>
2018-04-06 21:16:41 -0300

    add comentarios

--- Seguranca/recaptcha/cadastrar.php
+++ Seguranca/recaptcha/cadastrar.php
@@ -3,8 +3,9 @@
 $email = $_POST['inputEmail'];
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch,CURLOPT_SSL_VERIFYHOST, false); //seta como false a verificação de host
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //seta como false a verificação de pares
+
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
     'secret'=>"6LfGolEUAAAAAP_qrYT5njQlN9ymevlcJxwZzttl",
     'response'=> $_POST['g-recaptcha-response'],
@@ -14,12 +15,12 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 //echo json_encode(curl_exec($ch));
-$response = json_decode(curl_exec($ch), true);
+$response = json_decode(curl_exec($ch), true); //transforma de json pra array
 if ($response["success"] === true){
     echo "OK: $email";
 }
 else{
-    header("Location: reCaptcha.php");
+    header("Location: reCaptcha.php"); // se response[success] for false ele manda pra tela recaptcha.php
 }
 curl_close($ch);
 ?>
